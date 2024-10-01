fccd758db8aab811e4195f16b8395e28e14c0bff
vikabovkun <bovkunvika76@gmail.com>
2024-10-01 20:24:14 +0300

    https://github.com/bovkunvika/Repository.git

---
+++ hello_vscode/js/script.js
@@ -0,0 +1,10 @@
+// Функція для додавання двох чисел
+function addNumbers(a, b) {
+	return a + b;
+}
+
+// Виклик функції з двома числами
+let result = addNumbers(5, 3);
+// Виведення результату в консоль
+console.log("Результат додавання: " + result);
+console.log('');
